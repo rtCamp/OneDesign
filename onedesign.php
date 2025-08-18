e887679e302acc2c76b852cb8fd6ccb27d75e737
Utsav Patel <utsav.patel1512@outlook.com>
2025-08-18 15:45:29 +0530

    chore: added proper plugin loading action and fallback if vendor is missing

--- onedesign.php
+++ onedesign.php
@@ -28,10 +28,65 @@ define( 'ONEDESIGN_BUILD_JS_DIR_PATH', trailingslashit( ONEDESIGN_BUILD_PATH ) .
 define( 'ONEDESIGN_BUILD_CSS_URI', trailingslashit( ONEDESIGN_BUILD_URI ) . 'css/' );
 define( 'ONEDESIGN_BUILD_CSS_DIR_PATH', trailingslashit( ONEDESIGN_BUILD_PATH ) . 'css/' );
 
+
+
+// if autoload file does not exist then show notice that you are running the plugin from github repo so you need to build assets and install composer dependencies.
+if ( ! file_exists( ONEDESIGN_DIR_PATH . '/vendor/autoload.php' ) ) {
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+		<div class="notice notice-error">
+			<p>
+				<?php
+				printf(
+					/* translators: %s is the plugin name. */
+					esc_html__( 'You are running the %s plugin from the GitHub repository. Please build the assets and install composer dependencies to use the plugin.', 'onedesign' ),
+					'<strong>' . esc_html__( 'OneDesign', 'onedesign' ) . '</strong>'
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s is the command to run. */
+					esc_html__( 'Run the following commands in the plugin directory: %s', 'onedesign' ),
+					'<code>composer install && npm install && npm run build:prod</code>'
+				);
+				?>
+			<p>
+				<?php
+				printf(
+					/* translators: %s is the plugin name. */
+					esc_html__( 'Please refer to the %s for more information.', 'onedesign' ),
+					sprintf(
+						'<a href="%s" target="_blank">%s</a>',
+						esc_url( 'https://github.com/rtCamp/OneDesign' ),
+						esc_html__( 'OneDesign GitHub repository', 'onedesign' )
+					)
+				);
+				?>
+			</p>
+		</div>
+			<?php
+		}
+	);
+	return;
+}
+
 // Load autoloader.
 if ( file_exists( ONEDESIGN_DIR_PATH . '/vendor/autoload.php' ) ) {
 	require_once ONEDESIGN_DIR_PATH . '/vendor/autoload.php';
 }
 
-// Initialize the plugin.
-\OneDesign\Plugin::get_instance();
+/**
+ * Load the plugin.
+ */
+function onedesign_plugin_loader() {
+	\OneDesign\Plugin::get_instance();
+
+	// load plugin text domain.
+	load_plugin_textdomain( 'onedesign', false, ONEDESIGN_DIR_PATH . '/languages/' );
+}
+
+add_action( 'plugins_loaded', 'onedesign_plugin_loader' );
