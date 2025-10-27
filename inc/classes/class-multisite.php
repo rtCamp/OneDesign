37bd8504413a4ed37ef09fdd9f1ca0e8249e1967
Utsav Patel <utsav.patel1512@outlook.com>
2025-10-27 10:25:49 +0530

    update: site details in governing site if its changes any how

--- inc/classes/class-multisite.php
+++ inc/classes/class-multisite.php
@@ -50,6 +50,9 @@ class Multisite {
 
 		// auto assign brand-site on new site creation if governing site is set.
 		add_action( 'wp_initialize_site', array( $this, 'assign_brand_site_on_new_site_creation' ), 10, 2 );
+
+		// listen to option changes for blogname, siteurl and home to update into governing site table.
+		add_action( 'updated_option', array( $this, 'update_site_details_in_governing_site_table' ), 10, 3 );
 	}
 
 	/**
@@ -148,6 +151,54 @@ class Multisite {
 
 			update_option( Constants::ONEDESIGN_SITE_TYPE, 'brand-site', false );
 
+			restore_current_blog();
+		}
+	}
+
+	/**
+	 * Update site details in governing site table on option changes.
+	 *
+	 * @param string $option_name The name of the updated option.
+	 * @param mixed  $old_value The old value of the option.
+	 * @param mixed  $new_value The new value of the option.
+	 *
+	 * @return void
+	 */
+	public function update_site_details_in_governing_site_table( string $option_name, $old_value, $new_value ): void {
+
+		$governing_site_id = get_site_option( Constants::ONEDESIGN_MULTISITE_GOVERNING_SITE, 0 );
+
+		// If governing site is not set or we are on governing site, return.
+		if ( ! $governing_site_id || get_current_blog_id() === (int) $governing_site_id ) {
+			return;
+		}
+
+		$relevant_options = array( 'blogname', 'siteurl', 'home' );
+		$current_site_id  = get_current_blog_id();
+
+		if ( in_array( $option_name, $relevant_options, true ) ) {
+			if ( ! switch_to_blog( (int) $governing_site_id ) ) {
+				return;
+			}
+
+			// get shared sites from governing site.
+			$shared_sites = get_option( Constants::ONEDESIGN_SHARED_SITES, array() );
+
+			foreach ( $shared_sites as &$site ) {
+				if ( (int) $site['id'] === $current_site_id ) {
+					if ( 'blogname' === $option_name ) {
+						$site['name'] = sanitize_text_field( $new_value );
+					} elseif ( in_array( $option_name, array( 'siteurl', 'home' ), true ) ) {
+						$site['url'] = esc_url_raw( $new_value );
+					}
+					break;
+				}
+			}
+
+			// save the updated shared_sites option.
+			update_option( Constants::ONEDESIGN_SHARED_SITES, $shared_sites, false );
+
+			// restore blog.
 			restore_current_blog();
 		}
 	}
